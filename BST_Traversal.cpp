f375b529fb4dceffe439e0d442ef743cb2e1f9bd
Anish Khadka <akhadka2@sfsu.edu>
2024-07-02 12:41:24 -0700

    BST Traversal DFS ( Pre-Order, In-Order and Post-Order )

---
+++ BST_Traversal.cpp
@@ -0,0 +1,158 @@
+//Binary Search Treem traversal technique
+// Pre-Order (NLR)
+// In-Order (LNR)
+// Post -Order (LRN)
+
+#include <iostream>
+using namespace std;
+
+class TreeNode{
+    public:
+    TreeNode *left;
+    TreeNode *right;
+    int value;
+    //Default Constrctor
+    TreeNode(){
+        value = 0;
+        left;
+        right;
+    }
+    //parameterized constructor
+    TreeNode(int v){
+        left = NULL;
+        right = NULL;
+        value = v;
+    }
+};
+
+class BST{
+    public:
+    TreeNode *root;
+    //Default constructor
+    BST(){
+        root = NULL;
+    };
+    //check if our BST is empty
+    bool isEmpty(){
+    if (root == NULL){
+        return true;
+    }
+    else{
+        return false;
+    }
+    }
+    
+    //insertion method
+    void insert(TreeNode *newNode){
+        if (isEmpty()){
+            root = newNode;
+            cout << " New node inserted as root node, value = " << newNode->value << endl;
+        }
+        else{
+            TreeNode *temp = root;
+            while(temp != NULL){
+                if (temp->value == newNode->value){
+                    cout << "duplicate element. cannot proceed" << endl;
+                    return;
+                }
+                else if(newNode->value < temp->value && temp->left == NULL){
+                    temp->left = newNode;
+                    cout << " new node inserted as left child, value = " << newNode->value << endl;
+                    break;
+                }
+                else if(newNode->value < temp->value){
+                    temp = temp->left;
+                    cout << "moved to the left child" << endl;
+                    
+                }
+                else if(newNode->value > temp->value && temp->right == NULL){
+                    temp->right = newNode;
+                    cout << " new node inserted as right child, value = " << newNode->value << endl;
+                    break;
+                }
+                else if(newNode->value > temp->value){
+                    temp = temp->right;
+                    cout << " moved to the right child" << endl;
+                    
+                }
+            }
+        }
+    }
+    
+    // Traversal of BST, Pre-Order (Node -> Left -> Right)
+    void PreOrderTraversal(TreeNode *Node){
+        if (Node == NULL){
+            return;
+        }
+        cout << Node->value << " ";
+        PreOrderTraversal(Node -> left);
+        PreOrderTraversal(Node -> right);
+    }
+    
+    // Traversal of BST, In-Order (Left -> Node -> Right)
+    void InOrderTraversal(TreeNode *Node){
+        if (Node == NULL){
+            return;
+        }
+        else{
+            InOrderTraversal(Node -> left);
+        }
+        cout << Node -> value << " ";
+        InOrderTraversal(Node -> right);
+        
+    }
+    
+    //Traversal of BST, Post-Order (Left -> Right -> Node)
+    void PostOrderTraversal (TreeNode *Node){
+        if (Node == NULL){
+            return;
+        }
+        PostOrderTraversal( Node -> left);
+        PostOrderTraversal (Node -> right);
+        cout << Node -> value << " ";
+        
+    }
+};
+
+int main()
+{
+    BST bst; // create a BST object named bst
+    //cout << bst.isEmpty();
+    TreeNode *newNode1 = new TreeNode();
+    TreeNode *newNode2 = new TreeNode();
+    TreeNode *newNode3 = new TreeNode();
+    TreeNode *newNode4 = new TreeNode();
+    TreeNode *newNode5 = new TreeNode();
+    TreeNode *newNode6 = new TreeNode();
+    int val1, val2, val3, val4, val5, val6;
+    cout << "enter the value of the new node" << endl;
+    cin >> val1;
+    cin >> val2;
+    cin >> val3;
+    cin >> val4;
+    cin >> val5;
+    cin >> val6;
+    newNode1 -> value = val1;
+    newNode2 -> value = val2;
+    newNode3 -> value = val3;
+    newNode4 -> value = val4;
+    newNode5 -> value = val5;
+    newNode6 -> value = val6;
+    bst.insert(newNode1);
+    bst.insert(newNode2);
+    bst.insert(newNode3);
+    bst.insert(newNode4);
+    bst.insert(newNode5);
+    bst.insert(newNode6);
+    cout << endl;
+    cout << "Printing our BST in Pre-order traversal: " << endl;
+    bst.PreOrderTraversal(newNode1);
+    cout << endl;
+    cout << "Printing our BST in In-order traversal: " << endl;
+    bst.InOrderTraversal(newNode1);
+    cout << endl;
+    cout << "Printing our BST in Post-order traversal: " << endl;
+    bst.PostOrderTraversal(newNode1);
+    
+    return 0;
+}
